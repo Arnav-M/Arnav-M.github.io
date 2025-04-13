f56be5779c75f45ec43c697d2772b213e9644be6
Arnav Mathur <75835285+Arnav-M@users.noreply.github.com>
2025-04-13 03:31:39 -0700

    changessss

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // script.js
 function startCountdown(duration) {
-    const countdownElement = document.getElementById('countdown');
     const hoursElement = document.getElementById('hours');
     const minutesElement = document.getElementById('minutes');
     const secondsElement = document.getElementById('seconds');
@@ -12,9 +11,9 @@ function startCountdown(duration) {
         minutes = Math.floor((timer % (60 * 60)) / 60);
         seconds = Math.floor(timer % 60);
 
-        hoursElement.textContent = `${hours} Hours`;
-        minutesElement.textContent = `${minutes} Minutes`;
-        secondsElement.textContent = `${seconds} Seconds`;
+        hoursElement.textContent = String(hours).padStart(2, '0');
+        minutesElement.textContent = String(minutes).padStart(2, '0');
+        secondsElement.textContent = String(seconds).padStart(2, '0');
 
         if (--timer < 0) {
             timer = 0;
